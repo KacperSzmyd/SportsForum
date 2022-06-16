442f0b6e7f4cc336794fe8f1a9cefef62e466d9d
Kacper Szmyd <s210737@student.uek.krakow.pl>
2022-06-16 22:34:31 +0200

    basic CRUD, searching for topics/rooms

--- base/views.py
+++ base/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Room, Topic, Message, User
+from django.db.models import Q
 from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    query = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.all()
+    rooms = Room.objects.filter(Q(name__icontains=query) |
+                                Q(topic__name__icontains=query) |
+                                Q(description__icontains=query) |
+                                Q(host__username__icontains=query))
+
+    context = {'rooms': rooms,
+               'topics': topics}
     return render(request, 'home.html', context)
 
 
@@ -48,3 +56,31 @@ def create_room(request):
                'page': page,
                'topics': topics}
     return render(request, 'room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    topics = Topic.objects.all()
+    room_form = RoomForm(instance=room)
+    if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.topic = topic
+        room.name = request.POST.get('name')
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('room', pk)
+
+    context = {'room': room,
+               'room_form': room_form,
+               'topics': topics}
+    return render(request, 'room_form.html', context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    context = {'room': room}
+    return render(request, 'delete.html', context)
